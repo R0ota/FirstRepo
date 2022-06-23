f21a5d0ae62906a832975b511ed128431af42486
R0ota <reham.mohamed.ma@gmail.com>
2022-06-23 21:44:15 +0300

    add challenge

---
+++ challeng .js
@@ -0,0 +1,79 @@
+
+// /*
+
+// // challeng 2 part 2 
+
+
+
+// // const bill=430;
+// // const tip=bill >=50 && bill<=300 ? bill*0.15:
+// // bill*0.20;
+
+
+
+// // const calctip =function(bill) {
+
+// //    return bill >=50 && bill<=300 ? bill*0.15:bill*0.20;
+
+
+// // }
+
+// // const bills= [125,555,44];
+
+
+
+// // //  const biil1 =calctip(bills[0]);
+// // //  const biil2 =calctip(bills[1]);
+// // //  const biil3 =calctip(bills[2]);
+// // const tips=[calctip(bills[0]),calctip(bills[1]),calctip(bills[2])  ];
+
+
+
+// // console.log(tips,bills);
+
+
+
+
+// */
+
+
+// //  loop for
+
+
+// // chalege 4 part 2
+
+// // cahallengr 4 part2
+
+
+// // const calctip =function(bill) {
+
+// //     return bill >=50 && bill<=300 ? bill*0.15:bill*0.20;
+
+
+// // }
+
+// //  const bills =[22, 295, 176, 440, 37, 105, 10, 1100, 86,52];
+
+// //  const tips =[];
+// // const totals =[];
+
+// // for(let i=0;i<bills.length;i++) {
+// //     const tip =calctip(bills[i]);
+// //     tips.push(tip);
+// //     totals.push(tip+bills[i])
+// // }
+
+
+// // console.log(bills,totals,tips)
+
+// // const calcage =function(arr) {
+// //     let sum=0;
+// //     for(i=0;i<arr.length;i++) {
+// //         sum+=arr[i];
+
+// //     }
+// // return sum/arr.length;
+// // } console.log(calcage([2,3,7]));
+
+//  console.log(calcage(tips));
+//  console.log(calcage(totals));
